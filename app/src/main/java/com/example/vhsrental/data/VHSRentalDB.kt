aa6cf6ed5ed7c0e0dceaf688503b6163501fbedc
hulka <xhulka@fi.muni.cz>
2024-04-20 10:51:05 +0200

    feat: add main db class

---
+++ app/src/main/java/com/example/vhsrental/data/VHSRentalDB.kt
@@ -0,0 +1,18 @@
+package com.example.vhsrental.data
+
+import android.content.Context
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import dagger.hilt.android.qualifiers.ActivityContext
+import javax.inject.Inject
+
+class VHSRentalDB @Inject constructor(@ActivityContext context: Context) {
+    private val driver: SqlDriver
+    init{
+        driver = AndroidSqliteDriver(
+            schema = VHSRental.Schema,
+            context = context,
+            name = "vhsrental.db"
+        )
+    }
+}
