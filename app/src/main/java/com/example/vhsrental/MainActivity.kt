4965b5a71a2efb6a2ec1d2d199cf239c3c505ab1
hulka <xhulka@fi.muni.cz>
2024-04-20 10:51:21 +0200

    fix: correct way to init viewmodel

--- app/src/main/java/com/example/vhsrental/MainActivity.kt
+++ app/src/main/java/com/example/vhsrental/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vhsrental
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.vhsrental.ui.MainActivityViewModel
 import com.example.vhsrental.ui.theme.VhsRentalTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val myViewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
