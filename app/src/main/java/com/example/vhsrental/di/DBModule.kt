a77ceaa83cc2ac61459b7cfa75132f9c4410e3a7
hulka <xhulka@fi.muni.cz>
2024-04-25 17:35:28 +0200

    fix: correct handling of application context providing

--- app/src/main/java/com/example/vhsrental/di/DBModule.kt
+++ app/src/main/java/com/example/vhsrental/di/DBModule.kt
@@ -1,11 +1,12 @@
 package com.example.vhsrental.di
 
 import android.content.Context
+import com.example.vhsrental.ui.MainActivity
 import com.example.vhsrental.data.VHSRentalDB
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
@@ -13,7 +14,6 @@ import dagger.hilt.components.SingletonComponent
 class DBModule {
 
     @Provides
-    fun provideDB(@ActivityContext context: Context) : VHSRentalDB {
+    fun provideDB(@ApplicationContext context: Context) : VHSRentalDB {
         return VHSRentalDB(context)
-    }
-}
+    }}
