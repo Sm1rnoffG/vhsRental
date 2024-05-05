483019a1ebf24e2493fa8b3506daef6338d20090
hulka <xhulka@fi.muni.cz>
2024-05-05 18:10:34 +0200

    refactor: migrate to string res

--- app/src/main/java/com/example/vhsrental/ui/screens/login/PasswordUpdateScreen.kt
+++ app/src/main/java/com/example/vhsrental/ui/screens/login/PasswordUpdateScreen.kt
@@ -3,68 +3,65 @@ package com.example.vhsrental.ui.screens.login
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import com.example.vhsrental.R
 import com.example.vhsrental.ui.viewmodels.LoginUiState
 import com.example.vhsrental.ui.viewmodels.UpdateAccountActions
 
 @Composable
 fun PasswordUpdateScreen(
     loggedIn: LoginUiState.LoggedIn,
-    onValueChange: (UpdateAccountActions) -> Unit,
-    onConfirm: () -> Unit,
-    onSuccess: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onValueChange: (UpdateAccountActions) -> Unit = {},
+    onConfirm: () -> Unit = {},
+    onSuccess: () -> Unit = {},
 ) {
     when {
         loggedIn.successfulUpdate -> {
-            SuccessfulUpdateAlert("Password successfully changed") { onSuccess() }
+            SuccessfulUpdateAlert(stringResource(id = R.string.password_alert)) { onSuccess() }
         }
     }
 
     Column (
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
+        modifier = modifier,
     ) {
         Text(
-            text = "Update password",
+            text = stringResource(id = R.string.update_password_heading),
             textAlign = TextAlign.Center
         )
         OutlinedTextField(
             value = loggedIn.newPassword,
-            label = { Text(text = "New password") },
+            label = { Text(text = stringResource(R.string.new_password_label)) },
             onValueChange = { onValueChange(UpdateAccountActions.OnPasswordUpdate(it)) },
             visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier,
-            isError = loggedIn.isError
+            isError = loggedIn.isError,
         )
         OutlinedTextField(
             value = loggedIn.newPasswordRepeat,
-            label = { Text(text = "Repeat new password") },
+            label = { Text(text = stringResource(id = R.string.repeat_new_password_label)) },
             onValueChange = { onValueChange(UpdateAccountActions.OnPasswordRepeatUpdate(it)) },
             visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier,
-            isError = loggedIn.isError
+            isError = loggedIn.isError,
         )
         OutlinedTextField(
             value = loggedIn.oldPassword,
-            label = { Text(text = "Old password") },
+            label = { Text(text = stringResource(R.string.old_password_label)) },
             onValueChange = { onValueChange(UpdateAccountActions.OnOldPasswordUpdate(it)) },
             visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier,
-            isError = loggedIn.isError
+            isError = loggedIn.isError,
         )
         if (loggedIn.isError) {
             Text(text = loggedIn.errorMessage)
@@ -72,11 +69,11 @@ fun PasswordUpdateScreen(
         Button(
             onClick = onConfirm,
             modifier = Modifier
-                .wrapContentSize()
+                .wrapContentSize(),
         ) {
             Text(
-                text = "Confirm changes",
-                textAlign = TextAlign.Center
+                text = stringResource(id = R.string.confirm_changes_button),
+                textAlign = TextAlign.Center,
             )
         }
     }
