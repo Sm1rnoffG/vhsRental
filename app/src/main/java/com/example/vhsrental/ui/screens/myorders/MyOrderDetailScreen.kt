9f3c72b0a775deb841aa95d6606dfddea12865d0
František Hůlka <xhulka@fi.muni.cz>
2024-05-18 08:16:20 +0200

    feat: small visual changes to detail of my order

--- app/src/main/java/com/example/vhsrental/ui/screens/myorders/MyOrderDetailScreen.kt
+++ app/src/main/java/com/example/vhsrental/ui/screens/myorders/MyOrderDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.example.vhsrental.ui.screens.myorders
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
@@ -10,10 +11,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import com.example.vhsrental.data.models.DomainMovie
 import com.example.vhsrental.data.models.DomainOrder
 import com.example.vhsrental.data.models.OrderState
 import com.example.vhsrental.ui.screens.movies.Poster
+import com.example.vhsrental.ui.theme.Paddings
 import com.example.vhsrental.ui.viewmodels.OrderActions
 
 @Composable
@@ -28,23 +31,37 @@ fun MyOrderDetailScreen(
         modifier = modifier.fillMaxWidth()
     ) {
         items (count = 1) {
-            Poster(url = movie?.imageUrl ?: "", isPreview = false)
+            Poster(
+                url = movie?.imageUrl ?: "",
+                isPreview = false,
+                modifier = Modifier
+                    .padding(Paddings.small)
+            )
             Text(
                 text = movie?.name ?: "",
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                fontSize = 35.sp,
+                modifier = Modifier
+                    .padding(Paddings.small)
             )
             Text(
                 text = order?.state?.name ?: "",
                 textAlign = TextAlign.Center,
-                color = if (order?.state == OrderState.OverDue) Color.Red else Color.Black
+                color = if (order?.state == OrderState.OverDue) Color.Red else Color.Black,
+                modifier = Modifier
+                    .padding(Paddings.small)
             )
             Text(
                 text = "From: ${order?.createDate}",
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(Paddings.small)
             )
             Text(
                 text = "Till: ${order?.returnDate ?: order?.createDate?.plusDays(3)}",
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(Paddings.small)
             )
             MyOrderActionButton(order, { action -> onMyOrderAction(action, movie) })
         }
