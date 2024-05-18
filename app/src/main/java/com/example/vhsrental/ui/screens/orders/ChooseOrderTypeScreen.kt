5942be2bd58e98a76d7bf55a964a9928b5571e8b
František Hůlka <xhulka@fi.muni.cz>
2024-05-18 08:26:10 +0200

    refactor: decompo order type selection and add larger padding

--- app/src/main/java/com/example/vhsrental/ui/screens/orders/ChooseOrderTypeScreen.kt
+++ app/src/main/java/com/example/vhsrental/ui/screens/orders/ChooseOrderTypeScreen.kt
@@ -1,18 +1,21 @@
 package com.example.vhsrental.ui.screens.orders
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.vhsrental.ui.theme.Paddings
 
 @Composable
@@ -21,30 +24,54 @@ fun ChooseOrderTypeScreen(
     isNewOrder: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val buttonModifier = Modifier
+        .padding(Paddings.large)
+        .fillMaxWidth()
+        .fillMaxHeight(0.5F)
+
     Column (
         modifier = modifier.fillMaxSize()
     ) {
-        Surface (
-            onClick = isReservation,
-            shape = RoundedCornerShape(5.dp),
+        OrderTypeButton(
+            text = "Create From Reservation",
             color = Color.Blue,
-            modifier = Modifier
-                .padding(Paddings.small)
-                .fillMaxWidth()
-                .fillMaxHeight(0.5F)
-        ) {
-            Text(text = "Create From Reservation", color = Color.White)
-        }
-        Surface (
-            onClick = isNewOrder,
-            shape = RoundedCornerShape(5.dp),
+            textColor = Color.Red,
+            onClick = isReservation,
+            modifier = buttonModifier
+        )
+        OrderTypeButton(
+            text = "Create New Order",
             color = Color.Red,
-            modifier = Modifier
-                .padding(Paddings.small)
-                .fillMaxWidth()
-                .fillMaxHeight(0.5F)
+            textColor = Color.White,
+            onClick = isNewOrder,
+            modifier = buttonModifier,
+        )
+    }
+}
+
+@Composable
+fun OrderTypeButton (
+    text: String,
+    color: Color,
+    textColor: Color,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Surface (
+        onClick = onClick,
+        shape = RoundedCornerShape(5.dp),
+        color = color,
+        modifier = modifier
+    ) {
+        Box (
+            contentAlignment = Alignment.Center
         ) {
-            Text(text = "Create New Order", color = Color.White)
+            Text(
+                text = text,
+                color = textColor,
+                fontSize = 35.sp,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
